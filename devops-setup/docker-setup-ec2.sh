96e32bbe7706a533496c6b963046025f85a4a52b
Gaurav kumar <gauravkumardeal@gmail.com>
2026-09-22 23:32:02 +0530

    update Docker Setup file

--- devops-setup/docker-setup-ec2.sh
+++ devops-setup/docker-setup-ec2.sh
@@ -1,16 +1,9 @@
+sudo yum update 
 sudo yum install docker -y
-
-
-
-
-sudo dnf install -y yum-utils device-mapper-persistent-data lvm2
-
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
-
-sudo systemctl start docker
-
+sudo yum install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
 sudo systemctl enable docker
-
+sudo systemctl start docker
 sudo systemctl status docker
+# Docker Setup Ready
